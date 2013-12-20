d48e4225178f4ffd83c2bafdec047a6f628f1dee
Justin McNally <justin@kohactive.com>
2013-12-20 12:24:38 -0500

    add http lib

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -1,6 +1,7 @@
 require 'capistrano'
 require 'capistrano/log_with_awesome'
 require 'json'
+require 'net/http'
 # TODO need to handle loading a bit beter. these would load into the instance if it's defined
 module Capistrano
   module Slack
