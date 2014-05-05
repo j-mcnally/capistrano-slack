1106cd8600154236e85eb1c000859eb6168df9b9
Joshua Puetz <josh@grorichpuetz.com>
2014-05-05 16:06:36 -0700

    Fix indentation

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -67,8 +67,8 @@ module Capistrano
                 slack_token = fetch(:slack_token)
                 slack_room = fetch(:slack_room)
                 slack_emoji = fetch(:slack_emoji) || ":ghost:"
-		            slack_username = fetch(:slack_username) || "deploybot"
-		            slack_application = fetch(:slack_application) || application
+                slack_username = fetch(:slack_username) || "deploybot"
+                slack_application = fetch(:slack_application) || application
                 slack_subdomain = fetch(:slack_subdomain)
                 return if slack_token.nil?
                 announced_deployer = fetch(:deployer)
