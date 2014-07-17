0aa3c44a5a8fb15b7d2d69fd5ccd19ca21e2d939
Benji Stein <bstein@siftscience.com>
2014-07-16 18:00:22 -0700

    Only print deploy message at code upload state, and print failed deploys.

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -9,7 +9,8 @@ module Capistrano
     def self.extended(configuration)
       configuration.load do
 
-        before 'deploy', 'slack:starting'
+
+        before 'deploy:update', 'slack:starting'
         before 'deploy:migrations', 'slack:configure_for_migrations', 'slack:starting'
         after 'deploy',  'slack:finished'
         after 'deploy:migrations', 'slack:configure_for_migrations', 'slack:finished'
@@ -19,92 +20,84 @@ module Capistrano
         end
         set :slack_with_migrations, false
 
+        deploy_failed_msg = lambda do
+          announcement = "#{@announced_deployer} cancelled deployment of #{@slack_application}"
+          announcement << "'s #{fetch(:branch)}" if fetch(:branch, nil)
+          announcement << " to #{@announced_stage}"
+        end
+
+        deploy_start_msg = lambda do
+          announcement = "#{@announced_deployer} is deploying #{@slack_application}"
+          announcement << "'s #{fetch(:branch)}" if fetch(:branch, nil)
+          announcement << " with migrations" if slack_with_migrations
+          announcement << " to #{@announced_stage}"
+        end
+
+        deploy_finished_msg = lambda do
+          announcement = "#{@announced_deployer} deployed #{@slack_application}"
+          announcement << "'s #{fetch(:branch)}" if fetch(:branch, nil)
+          announcement << " with migrations" if slack_with_migrations
+          announcement << " to #{@announced_stage}"
+          announcement << " to #{@announced_stage} successfully in #{@elapsed} seconds"
+        end
+
+        def send_slack(msg)
+          begin
+            @slack_token = fetch(:slack_token)
+            @slack_room = fetch(:slack_room)
+            @slack_emoji = fetch(:slack_emoji) rescue nil || ":ghost:"
+            @slack_username = fetch(:slack_username) rescue nil || "deploybot"
+            @slack_application = fetch(:slack_application) rescue nil || application || "unknown"
+            @slack_subdomain = fetch(:slack_subdomain)
+            return if slack_token.nil?
+            @announced_deployer = ActiveSupport::Multibyte::Chars.new(fetch(:deployer)).mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').to_s
+            @announced_stage = fetch(:stage, 'production')
+            @elapsed = fetch(:elapsed, "")
+
+            announcement = msg.call
+
+            # Parse the API url and create an SSL connection
+            uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
+            http = Net::HTTP.new(uri.host, uri.port)
+            http.use_ssl = true
+            http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+            # Create the post request and setup the form data
+            request = Net::HTTP::Post.new(uri.request_uri)
+            request.set_form_data(:payload => {'channel' => @slack_room, 'username' => @slack_username, 'text' => announcement, "icon_emoji" => @slack_emoji}.to_json)
+
+            # Make the actual request to the API
+            response = http.request(request)
+          rescue Faraday::Error::ParsingError
+            # FIXME deal with crazy color output instead of rescuing
+            # it's stuff like: ^[[0;33m and ^[[0m
+          end
+        end
 
         namespace :slack do
             task :configure_for_migrations do
               set :slack_with_migrations, true
             end
-
             task :starting do
-              slack_token = fetch(:slack_token)
-              slack_room = fetch(:slack_room)
-              slack_emoji = fetch(:slack_emoji) || ":ghost:"
-              slack_username = fetch(:slack_username) || "deploybot"
-              slack_application = fetch(:slack_application) || application
-              slack_subdomain = fetch(:slack_subdomain)
-              return if slack_token.nil?
-              announced_deployer = ActiveSupport::Multibyte::Chars.new(fetch(:deployer)).mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/,'').to_s
-              announced_stage = fetch(:stage, 'production')
+              on_rollback do
+                set(:slack_emoji, ":no_entry:")
+                send_slack(deploy_failed_msg)
+              end
 
-              announcement = if fetch(:branch, nil)
-                               "#{announced_deployer} is deploying #{slack_application}'s #{branch}"
-                             else
-                               "#{announced_deployer} is deploying #{slack_application}"
-                             end
-              announcement << " with migrations" if slack_with_migrations
-              announcement << " to #{announced_stage}"
-
-
-              # Parse the API url and create an SSL connection
-              uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
-              http = Net::HTTP.new(uri.host, uri.port)
-              http.use_ssl = true
-              http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-
-              # Create the post request and setup the form data
-              request = Net::HTTP::Post.new(uri.request_uri)
-              request.set_form_data(:payload => {'channel' => slack_room, 'username' => slack_username, 'text' => announcement, "icon_emoji" => slack_emoji}.to_json)
-
-              # Make the actual request to the API
-              response = http.request(request)
-
+              send_slack(deploy_start_msg)
               set(:start_time, Time.now)
             end
 
 
             task :finished do
-              begin
-                slack_token = fetch(:slack_token)
-                slack_room = fetch(:slack_room)
-                slack_emoji = fetch(:slack_emoji) || ":ghost:"
-                slack_username = fetch(:slack_username) || "deploybot"
-                slack_application = fetch(:slack_application) || application
-                slack_subdomain = fetch(:slack_subdomain)
-                return if slack_token.nil?
-                announced_deployer = fetch(:deployer)
-                announced_stage = fetch(:stage, 'production')
-                end_time = Time.now
-                start_time = fetch(:start_time)
-                elapsed = end_time.to_i - start_time.to_i
+              end_time = Time.now
+              start_time = fetch(:start_time)
+              set(:elapsed, end_time.to_i - start_time.to_i)
 
-                msg = if fetch(:branch, nil)
-                         "#{announced_deployer} deployed #{slack_application}'s #{branch}"
-                       else
-                         "#{announced_deployer} deployed #{slack_application}"
-                       end
-                msg << " with migrations" if slack_with_migrations
-                msg << " to #{announced_stage} successfully in #{elapsed} seconds"
-
-                # Parse the URI and handle the https connection
-                uri = URI.parse("https://#{slack_subdomain}.slack.com/services/hooks/incoming-webhook?token=#{slack_token}")
-                http = Net::HTTP.new(uri.host, uri.port)
-                http.use_ssl = true
-                http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-                # Create the post request and setup the form data
-                request = Net::HTTP::Post.new(uri.request_uri)
-                request.set_form_data(:payload => {'channel' => slack_room, 'username' => slack_username, 'text' => msg, "icon_emoji" => slack_emoji}.to_json)
-
-                # Make the actual request to the API
-                response = http.request(request)
-
-              rescue Faraday::Error::ParsingError
-                # FIXME deal with crazy color output instead of rescuing
-                # it's stuff like: ^[[0;33m and ^[[0m
-              end
+              send_slack(deploy_finished_msg)
             end
-          end
         end
+      end
     end
   end
 end
