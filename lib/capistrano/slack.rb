892525d7588c89549479585c2b0597dfa72bd605
Chad Clark <chadsrad@gmail.com>
2014-08-19 11:06:39 -0700

    Fix for issue
    :slack_emoji is mandatory #9

--- lib/capistrano/slack.rb
+++ lib/capistrano/slack.rb
@@ -12,7 +12,7 @@ module Capistrano
         'channel' => fetch(:slack_room),
         'username' => fetch(:slack_username), 
         'text' => announcement, 
-        'icon_emoji' => fetch(:slack_emoji)
+        'icon_emoji' => fetch(:slack_emoji, '') 
         }.to_json
       end
 
